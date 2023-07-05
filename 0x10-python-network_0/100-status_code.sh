152cc884e18b041f84885c637f5a17b55852cb32
wisteen <wisdomisaac168@gmail.com>
2023-07-05 03:35:20 -0700

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-url=$1
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-echo "$status_code"
+# sends a request to a URL, and displays only the status code of the response.
+curl -s -o /dev/null -w "%{http_code}" "$1"
