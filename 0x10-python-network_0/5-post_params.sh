086f125fee693f6d57b917206fad289a2f0348de
wisteen <wisdomisaac168@gmail.com>
2023-07-05 02:56:23 -0700

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a DELETE request to the URL passed and display body of response
-curl -s -X "POST" "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+# Script that sends a POST request and displays the body response
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
